345e06e21af4f5c674371ed630f3ac83548483d4
axqa <max-ok@list.ru>
2024-04-04 17:24:57 +0300

    примеры

--- tests/test_strassen.py
+++ tests/test_strassen.py
@@ -16,8 +16,50 @@ class TestPiOA_3a(unittest.TestCase):
 
         res = strassen(matA, matB)
         expected = matA * matB
+        print(matA)
+        print(matB)
+        print(res)
+        print(expected)
         # self.assertEqual(strassen(matA, matB), matA * matB)
-        self.assertTrue(np.array_equal(res, expected))                            
+        self.assertTrue(np.array_equal(res, expected))   
+
+    def test1(self):
+        matA = np.array(
+            [[2, 4, 7, 3],
+            [3, 8, 1, 6],
+            [5, 1, 1, 5],
+            [4, 1, 6, 7]]
+        )                  
+        matB = np.array(
+            [[1, 2, 3, 4],
+            [8, 7, 6, 5],
+            [9, 0, 1, 2],
+            [9, 9, 4, 3]]
+        )
+
+        res = strassen(matA, matB)
+        expected = matA * matB
+        self.assertTrue(np.array_equal(res, expected))  
+
+    def test2(self):
+        matA = np.array(
+            [[3, 4, 2, 0],
+            [1, 4, 4, 3],
+            [0, 5, 6, 1],
+            [5, 9, 4, 3]]
+        )                  
+        matB = np.array(
+            [[0, 6, 4, 3]
+            [0, 9, 1, 0]
+            [5, 2, 1, 9]
+            [7, 0, 4, 3]]
+        )
+
+        res = strassen(matA, matB)
+        expected = matA * matB
+        self.assertTrue(np.array_equal(res, expected))  
+
+        
 if __name__ == '__main__':
     print(sys.path)
     # sys.path.insert(1, os.getcwd())
