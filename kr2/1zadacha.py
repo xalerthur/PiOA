3147b8f58188cde90a3725c29e27ebf28c6ebab5
xalerthur <117278890+xalerthur@users.noreply.github.com>
2024-05-16 21:06:06 +0300

    Обновление 1zadacha.py

--- kr2/1zadacha.py
+++ kr2/1zadacha.py
@@ -1,5 +1,6 @@
 def subset_sum_backtrack(numbers, target):
     def backtrack(start, path, target):
+        print("Шаг {}: Путь: {}, Оставшаяся сумма: {}".format(start, path, target))
         if target == 0:
             result.append(path)
             return
@@ -8,14 +9,20 @@ def subset_sum_backtrack(numbers, target):
         for i in range(start, len(numbers)):
             backtrack(i + 1, path + [numbers[i]], target - numbers[i])
 
+
     result = []
     backtrack(0, [], target)
     return result
 
 
+
+
 W = [2, 4, 7, 4, 1, 3]
 D = 12
 
+
+print("Исходный список чисел:", W)
+print("Целевая сумма:", D)
 result = subset_sum_backtrack(W, D)
 print("Возможные подмножества суммы", D, ":")
 for subset in result:
